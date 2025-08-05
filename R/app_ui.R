a3eb5b5b4fd9db8f0092350868eb3fdfb43cc0dc
Renan <renan.pascon@gmail.com>
2025-08-05 00:33:05 -0300

    test commit

--- R/app_ui.R
+++ R/app_ui.R
@@ -7,7 +7,6 @@
 app_ui <- function(request) {
 
   tagList(
-    # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
